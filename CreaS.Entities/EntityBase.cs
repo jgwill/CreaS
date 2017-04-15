ad71c30b3acb83713a9e8e6de5c3f440f9c6f90d
jgwill <jgi@jgwill.com>
2017-04-15 07:58:27 -0400

    slowly migrating my idea within this structure...

---
+++ CreaS.Entities/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Creas.Entities
+{
+  public abstract class EntityBase
+  {
+    protected Guid idug = Guid.Empty;
+
+    public virtual Guid IduGuid
+    {
+      get
+      {
+        if (idug == Guid.Empty) idug = Guid.NewGuid();
+        return idug;
+      }
+      set { idug = value; }
+    }
+
+
+  }
+}
